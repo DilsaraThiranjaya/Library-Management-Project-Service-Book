7f25d7888c02b81870b3e7cc15b8b7ebac0f6837
Dilsara Thiranjaya <dilsarathiranjaya3@gmail.com>
2026-03-18 09:34:21 +0530

    chore: commit all changes and push to all relevant remote repos

--- target/generated-sources/annotations/lk/ijse/eca/bookservice/mapper/BookMapperImpl.java
+++ target/generated-sources/annotations/lk/ijse/eca/bookservice/mapper/BookMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-16T09:11:31+0530",
+    date = "2026-03-16T12:22:07+0530",
     comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.2 (Oracle Corporation)"
 )
 @Component
